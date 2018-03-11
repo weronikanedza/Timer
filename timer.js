abd8551dd47c251e56f93f03231e7a5fc3ee828f
weronikanedza <weronika.nedza@int.pl>
2018-03-12 00:13:55 +0100

    Add files via upload

--- timer.js
+++ timer.js
@@ -3,9 +3,16 @@
     minutes:1,
     seconds:0,
     intervalID:null,
+    temp:0,
+    oneSecond:1000,
     };
 
 window.onload = function () {        
+
+   const timeElement=document.querySelector('#time');
+   const startButton=document.querySelector('#start');
+   const stopButton=document.querySelector('#stop');
+   const timerBlock=document.querySelector('#timer');
 
     document.getElementById("start").onclick=function(){
         document.getElementById("start").disabled = true;
@@ -17,22 +24,23 @@ window.onload = function () {
             }else if(timeParts.seconds!==0) {
                  timeParts.seconds--;
             }else{
-                document.getElementsByClassName("timer").style.backgroundColor="#90EE90";
-                document.getElementById("start").style.backgroundColor="#90EE90";
-                document.getElementById("stop").style.backgroundColor="#90EE90";
+                timerBlock.style.backgroundColor="#90EE90";
+                startButton.style.backgroundColor="#90EE90";
+                stopButton.style.backgroundColor="#90EE90";
             }
 
-            var temp=(timeParts.minutes<10)?"0"+timeParts.minutes+":":timeParts.minutes+":";
+            temp=(timeParts.minutes<10)?"0"+timeParts.minutes+":":timeParts.minutes+":";
+           // temp=(timeParts.minutes<10)?'0${timeParts.minutes} :':'${timeParts.minutes} :';
             temp+=(timeParts.seconds<10)?"0"+timeParts.seconds:timeParts.seconds;
                 
-            document.getElementById("time").innerHTML=temp;
-            document.getElementById("start").innerHTML="RESUME";
+            timeElement.innerHTML=temp;
+            startButton.innerHTML="RESUME";
                                
-    },1000)  
+    },timeParts.oneSecond)  
         };
 
-        document.getElementById("stop").onclick=function(){
-        document.getElementById("start").disabled = false;
-        clearInterval(intervalID);
-       };
+            document.getElementsByClassName("stop").onclick=function(){
+            document.getElementsByClassName("start").disabled=false;
+            clearInterval(intervalID);
+        };   
 }
